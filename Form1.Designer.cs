d16189749aba5c341cd8c151d02867e6f183a341
SATYAM MANDAL <satyam.mandal.77@gmail.com>
2021-06-09 02:11:41 +0530

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace SampleApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dbtext = new System.Windows.Forms.TextBox();
+            this.urltxt = new System.Windows.Forms.TextBox();
+            this.dbbtn = new System.Windows.Forms.Button();
+            this.urlchkbtn = new System.Windows.Forms.Button();
+            this.status = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dbtext
+            // 
+            this.dbtext.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.dbtext.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.HistoryList;
+            this.dbtext.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.dbtext.Location = new System.Drawing.Point(55, 41);
+            this.dbtext.Name = "dbtext";
+            this.dbtext.PlaceholderText = "Connect to Database";
+            this.dbtext.Size = new System.Drawing.Size(404, 27);
+            this.dbtext.TabIndex = 0;
+            // 
+            // urltxt
+            // 
+            this.urltxt.Location = new System.Drawing.Point(55, 293);
+            this.urltxt.Name = "urltxt";
+            this.urltxt.PlaceholderText = "Check Your URL";
+            this.urltxt.Size = new System.Drawing.Size(404, 27);
+            this.urltxt.TabIndex = 1;
+            // 
+            // dbbtn
+            // 
+            this.dbbtn.Location = new System.Drawing.Point(465, 38);
+            this.dbbtn.Name = "dbbtn";
+            this.dbbtn.Size = new System.Drawing.Size(107, 30);
+            this.dbbtn.TabIndex = 2;
+            this.dbbtn.Text = "Connect";
+            this.dbbtn.UseVisualStyleBackColor = true;
+            this.dbbtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // urlchkbtn
+            // 
+            this.urlchkbtn.Location = new System.Drawing.Point(466, 293);
+            this.urlchkbtn.Name = "urlchkbtn";
+            this.urlchkbtn.Size = new System.Drawing.Size(93, 27);
+            this.urlchkbtn.TabIndex = 3;
+            this.urlchkbtn.Text = "Check";
+            this.urlchkbtn.UseVisualStyleBackColor = true;
+            this.urlchkbtn.Click += new System.EventHandler(this.urlchkbtn_Click);
+            // 
+            // status
+            // 
+            this.status.AutoSize = true;
+            this.status.Location = new System.Drawing.Point(55, 433);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(52, 20);
+            this.status.TabIndex = 4;
+            this.status.Text = "Status:";
+            this.status.Click += new System.EventHandler(this.status_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(582, 553);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.urlchkbtn);
+            this.Controls.Add(this.dbbtn);
+            this.Controls.Add(this.urltxt);
+            this.Controls.Add(this.dbtext);
+            this.MaximumSize = new System.Drawing.Size(600, 600);
+            this.MinimumSize = new System.Drawing.Size(600, 600);
+            this.Name = "Form1";
+            this.RightToLeftLayout = true;
+            this.Text = "Url Check";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox dbtext;
+        private System.Windows.Forms.TextBox urltxt;
+        private System.Windows.Forms.Button dbbtn;
+        private System.Windows.Forms.Button urlchkbtn;
+        private System.Windows.Forms.Label status;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SampleApp
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            urltxt.Enabled = false;
+            urlchkbtn.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {/**
+            string connectionString = dbtext.Text;
+            SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-11GU0EI;Initial Catalog=demourl;Integrated Security=True");
+            SqlDataAdapter sqldata = new SqlDataAdapter("insert into urls (url,CreatedOn,ModifyOn,IsActive,IsDelete) values('"+dbtext.Text+"',GETDATE(),GETDATE(),1,0)", con);
+            DataTable table = new DataTable();
+            sqldata.Fill(table);
+            foreach(DataRow row in table.Rows)
+            {
+                status.Text = row["url"].ToString();
+            }
+            //status.Text = dt.ToString();
+       */
+            try {
+                string connectionstring = dbtext.Text;
+                con = new SqlConnection(@"" + connectionstring);
+                SqlDataAdapter sqldata = new SqlDataAdapter("select url from urls", con);
+                DataTable table = new DataTable();
+                sqldata.Fill(table);
+                urltxt.Enabled = true;
+                urlchkbtn.Enabled = true;
+                dbtext.Enabled = false;
+                status.Text = "Connection Stablished";
+            }
+            catch(Exception ex) {
+                Console.WriteLine(ex.Message);
+                status.Text = "Check Your Connection String";
+            }
+
+        }
+
+        private void urlchkbtn_Click(object sender, EventArgs e)
+        {
+            string url = urltxt.Text;
+            bool valid = IsUrlValid(url);
+            if (valid == true)
+            {
+                if (chktable(url) == true)
+                {
+                    status.Text = "Status: Already in Database";
+                }
+                else {
+                    SqlDataAdapter sqldata = new SqlDataAdapter("insert into urls (url,CreatedOn,ModifyOn,IsActive,IsDelete) values('" + url + "',GETDATE(),GETDATE(),1,0)", con);
+                    DataTable table = new DataTable();
+                    sqldata.Fill(table);
+                    status.Text = "Status: Valid Url And Add in Database";
+                }
+            }
+            else
+            {
+                status.Text = "Status: Invalid Valid Url";
+            }
+        }
+        public bool IsUrlValid(string webUrl)
+        {
+            if (webUrl == null || webUrl == "") return false;
+            return Regex.IsMatch(webUrl, @"(http|https)://(([www\.])?|([\da-z-\.]+))\.([a-z\.]{2,3})$*");
+        }
+
+       public bool chktable(string data)
+        {
+            SqlDataAdapter sqldata = new SqlDataAdapter("select url from urls where url='"+ data + "'", con);
+            DataTable table = new DataTable();
+            sqldata.Fill(table);
+            if (table != null) return true;
+            else return false;
+        }
+
+        private void status_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
